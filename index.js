2b998ab10212f15fb60c4e2482940b3e53c19d47
Hanna Engel <hengel@alumni.berklee.edu>
2019-06-06 11:38:52 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+
+function writeCards(array, event){
+    const a = []
+    for (let i = 0; i < array.length; i++) {
+    let  message = `Thank you, ${array[i]}, for the wonderful ${event} gift!`;
+    a.push(message);
+    }
+    return a;
+}
+function countdown(num){
+    let countdown = num 
+    while (countdown > -1) {
+        console.log(countdown--);
+    }
+}
